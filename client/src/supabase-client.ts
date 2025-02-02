a52911393a77067250f92c9c077d8daae432e86a
ilhanmaghriby <ilhan.maghriby23@gmail.com>
2025-02-02 16:05:22 +0700

    updated docs for supabase-client.ts

--- client/src/supabase-client.ts
+++ client/src/supabase-client.ts
@@ -1,8 +1,10 @@
 import { createClient, SupabaseClient } from "@supabase/supabase-js";
 
+// Supabase configuration
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_API_KEY;
 
+// Initialize Supabase
 const supabase: SupabaseClient = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
